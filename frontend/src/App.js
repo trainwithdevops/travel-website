04263d7d40d55f3a2ede3135714ec962a99a7c72
trainwithdevops <trainwithdevops@gmail.com>
2025-02-21 14:35:40 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,25 +9,39 @@ import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Profile from './pages/Profile';
 import Messages from './components/Messages';
+import Notifications from './components/Notifications';
+import TripPlanner from './components/TripPlanner';
+import GroupTrips from './components/GroupTrips';
 import Navbar from './components/Navbar';
 import PrivateRoute from './components/PrivateRoute';
+import { ThemeProvider } from '@material-ui/core/styles';
+import theme from './themes/theme';
+import './i18n';
+import { useTranslation } from 'react-i18next';
 
 function App() {
+    const { t } = useTranslation();
+
     return (
-        <Router>
-            <Navbar />
-            <Switch>
-                <Route exact path="/" component={Home} />
-                <Route path="/about-us" component={AboutUs} />
-                <Route path="/contact-us" component={ContactUs} />
-                <Route path="/login" component={Login} />
-                <Route path="/signup" component={Signup} />
-                <PrivateRoute path="/blog" component={Blog} />
-                <PrivateRoute path="/find-members" component={FindMembers} />
-                <PrivateRoute path="/profile" component={Profile} />
-                <PrivateRoute path="/messages" component={Messages} />
-            </Switch>
-        </Router>
+        <ThemeProvider theme={theme}>
+            <Router>
+                <Navbar />
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route path="/about-us" component={AboutUs} />
+                    <Route path="/contact-us" component={ContactUs} />
+                    <Route path="/login" component={Login} />
+                    <Route path="/signup" component={Signup} />
+                    <PrivateRoute path="/blog" component={Blog} />
+                    <PrivateRoute path="/find-members" component={FindMembers} />
+                    <PrivateRoute path="/profile" component={Profile} />
+                    <PrivateRoute path="/messages" component={Messages} />
+                    <PrivateRoute path="/notifications" component={Notifications} />
+                    <PrivateRoute path="/trip-planner" component={TripPlanner} />
+                    <PrivateRoute path="/group-trips" component={GroupTrips} />
+                </Switch>
+            </Router>
+        </ThemeProvider>
     );
 }
 
