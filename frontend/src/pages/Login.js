7dbcd21aabc6005f60e81aacce20bea9ebbb2db6
trainwithdevops <trainwithdevops@gmail.com>
2025-02-21 14:26:14 +0530

    Create Login.js

---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div>
+            <h1>Login</h1>
+            {/* Implement login form */}
+        </div>
+    );
+};
+
+export default Login;
