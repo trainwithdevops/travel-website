42ee95f019085451f4dc41c15ecdda651c1bf9fb
trainwithdevops <trainwithdevops@gmail.com>
2025-02-21 14:37:19 +0530

    Create theme.js

---
+++ frontend/src/themes/theme.js
@@ -0,0 +1,17 @@
+import { createTheme } from '@material-ui/core/styles';
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#3f51b5',
+        },
+        secondary: {
+            main: '#f50057',
+        },
+    },
+    typography: {
+        fontFamily: 'Roboto, Arial, sans-serif',
+    },
+});
+
+export default theme;
