ca691ce4f434f2f2d17129e2657d8c2ca67336ea
trainwithdevops <trainwithdevops@gmail.com>
2025-02-21 14:25:33 +0530

    Create AboutUs.js

---
+++ frontend/src/pages/AboutUs.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AboutUs = () => {
+    return (
+        <div>
+            <h1>About Us</h1>
+            <p>We are a community of travellers who love to explore the world.</p>
+        </div>
+    );
+};
+
+export default AboutUs;
