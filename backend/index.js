fefdc429ef16ff436c6e8b371f1ac5b79be4d428
trainwithdevops <trainwithdevops@gmail.com>
2025-02-21 14:21:58 +0530

    Create index.js

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const userRoutes = require('./routes/userRoutes');
+const blogRoutes = require('./routes/blogRoutes');
+const memberRoutes = require('./routes/memberRoutes');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.log(err));
+
+app.use('/api/users', userRoutes);
+app.use('/api/blogs', blogRoutes);
+app.use('/api/members', memberRoutes);
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
