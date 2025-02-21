0caf8fd110bb4bd32729a488ef860f4e86c7fe94
trainwithdevops <trainwithdevops@gmail.com>
2025-02-21 14:27:31 +0530

    Create FindMembers.js

---
+++ frontend/src/pages/FindMembers.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const FindMembers = () => {
+    return (
+        <div>
+            <h1>Find Members</h1>
+            <p>Find and connect with other travellers.</p>
+            {/* Implement filtering options */}
+        </div>
+    );
+};
+
+export default FindMembers;
