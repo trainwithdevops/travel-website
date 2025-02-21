2d727c1e30fc519c210276b15621210762261c7f
trainwithdevops <trainwithdevops@gmail.com>
2025-02-21 14:25:06 +0530

    Create Home.js

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Welcome to the Travel Community</h1>
+            <p>Connect with like-minded travellers and plan your future trips.</p>
+        </div>
+    );
+};
+
+export default Home;
