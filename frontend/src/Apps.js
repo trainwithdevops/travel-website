5123de4ec2b759b656842eaacb5660a75494bf80
trainwithdevops <trainwithdevops@gmail.com>
2025-02-21 14:23:57 +0530

    Create Apps.js

---
+++ frontend/src/Apps.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+import AboutUs from './pages/AboutUs';
+import ContactUs from './pages/ContactUs';
+import Blog from './pages/Blog';
+import FindMembers from './pages/FindMembers';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Navbar from './components/Navbar';
+import PrivateRoute from './components/PrivateRoute';
+
+function App() {
+    return (
+        <Router>
+            <Navbar />
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route path="/about-us" component={AboutUs} />
+                <Route path="/contact-us" component={ContactUs} />
+                <Route path="/login" component={Login} />
+                <Route path="/signup" component={Signup} />
+                <PrivateRoute path="/blog" component={Blog} />
+                <PrivateRoute path="/find-members" component={FindMembers} />
+            </Switch>
+        </Router>
+    );
+}
+
+export default App;
