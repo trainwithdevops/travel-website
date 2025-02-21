2ed4416b8da1525568e9bd813f9eecf7c15af69b
trainwithdevops <trainwithdevops@gmail.com>
2025-02-21 14:26:30 +0530

    Create SignUp.js

---
+++ frontend/src/pages/SignUp.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Signup = () => {
+    return (
+        <div>
+            <h1>Signup</h1>
+            {/* Implement signup form */}
+        </div>
+    );
+};
+
+export default Signup;
